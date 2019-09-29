4c0dafb56d5f06dcfb0612cfc3e1d23ddb406843
chapaikaNK <52287604+chapaikaNK@users.noreply.github.com>
2019-09-29 14:20:02 +0300

    реализовал открытие map popup доделал index.html

---
+++ js/popup-open-close.js
@@ -0,0 +1,9 @@
+function mapPopup() {
+  var popup = document.getElementById("map-popup-wrapper");
+  document.getElementById("map-popup-wrapper").className = "map-popup-wrapper show"
+}
+
+function mapPopupClose() {
+  var popup = document.getElementById("map-popup-wrapper");
+  document.getElementById("map-popup-wrapper").className = "map-popup-wrapper"
+}
